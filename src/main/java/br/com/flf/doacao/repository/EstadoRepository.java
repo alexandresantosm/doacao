37e6fde8cd6c85750aadea3454f6e757d4e34977
alexandresantosm <amenezes031@gmail.com>
2020-05-24 17:13:46 -0300

    Adicionando anotação nas classes de repositório de cidades e de estados.

--- src/main/java/br/com/flf/doacao/repository/EstadoRepository.java
+++ src/main/java/br/com/flf/doacao/repository/EstadoRepository.java
@@ -1,9 +1,11 @@
 package br.com.flf.doacao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.flf.doacao.domain.Estado;
 
+@Repository
 public interface EstadoRepository extends JpaRepository<Estado, Long> {
 
 }
