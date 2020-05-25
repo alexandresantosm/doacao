eda47fb30c53cbb31c1224d562bd4f05a271a8d5
alexandresantosm <amenezes031@gmail.com>
2020-05-25 00:02:34 -0300

    Adição do mapeamento para o repositório de endereço.

--- src/main/java/br/com/flf/doacao/repository/EnderecoRepository.java
+++ src/main/java/br/com/flf/doacao/repository/EnderecoRepository.java
@@ -1,9 +1,11 @@
 package br.com.flf.doacao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.flf.doacao.domain.Endereco;
 
+@Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 
 }
